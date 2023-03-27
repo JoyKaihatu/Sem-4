ce4fde33587b1ec711121c10d0411496c614c7e5
Joy <joykaihatu13@gmail.com>
2023-03-27 18:35:28 +0700

    Single Digit Hand-written Image

--- KB/Cakaran.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-np.random.rand()
-
-np.dot([0, 0, 1, 2, 3], [1, 2, 0, 1, 2])
---
+++ KB/Neural_Network_Project/Cakaran.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import pickle
+data = pd.read_csv('Digit-Recognizer_Dataset/Models.csv')
+
